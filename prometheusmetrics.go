41d551ac67c1cd68d2d16351a2078eea9a436798
Nate Brown <nbrown.us@gmail.com>
2018-06-22 13:19:51 -0700

    Support histograms

--- prometheusmetrics.go
+++ prometheusmetrics.go
@@ -18,6 +18,7 @@ type PrometheusConfig struct {
 	promRegistry  prometheus.Registerer //Prometheus registry
 	FlushInterval time.Duration         //interval to update prom metrics
 	gauges        map[string]prometheus.Gauge
+	gaugeVecs     map[string]prometheus.GaugeVec
 }
 
 // NewPrometheusProvider returns a Provider that produces Prometheus metrics.
@@ -30,6 +31,7 @@ func NewPrometheusProvider(r metrics.Registry, namespace string, subsystem strin
 		promRegistry:  promRegistry,
 		FlushInterval: FlushInterval,
 		gauges:        make(map[string]prometheus.Gauge),
+		gaugeVecs:     make(map[string]prometheus.GaugeVec),
 	}
 }
 
@@ -56,6 +58,36 @@ func (c *PrometheusConfig) gaugeFromNameAndValue(name string, val float64) {
 	}
 	g.Set(val)
 }
+
+func (c *PrometheusConfig) histogramFromName(name string, snap metrics.Histogram) {
+	key := fmt.Sprintf("%s_%s_%s", c.namespace, c.subsystem, name)
+	g, ok := c.gaugeVecs[key]
+	if !ok {
+		g = *prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: c.flattenKey(c.namespace),
+			Subsystem: c.flattenKey(c.subsystem),
+			Name:      c.flattenKey(name),
+			Help:      name,
+		},
+			[]string{
+				"type",
+			},
+		)
+		c.promRegistry.MustRegister(g)
+		c.gaugeVecs[key] = g
+	}
+
+	g.WithLabelValues("count").Set(float64(snap.Count()))
+	g.WithLabelValues("max").Set(float64(snap.Max()))
+	g.WithLabelValues("min").Set(float64(snap.Min()))
+	g.WithLabelValues("mean").Set(snap.Mean())
+	g.WithLabelValues("stddev").Set(snap.StdDev())
+	g.WithLabelValues("perc75").Set(snap.Percentile(float64(75)))
+	g.WithLabelValues("perc95").Set(snap.Percentile(float64(95)))
+	g.WithLabelValues("perc99").Set(snap.Percentile(float64(99)))
+	g.WithLabelValues("perc999").Set(snap.Percentile(float64(99.9)))
+}
+
 func (c *PrometheusConfig) UpdatePrometheusMetrics() {
 	for _ = range time.Tick(c.FlushInterval) {
 		c.UpdatePrometheusMetricsOnce()
@@ -72,11 +104,8 @@ func (c *PrometheusConfig) UpdatePrometheusMetricsOnce() error {
 		case metrics.GaugeFloat64:
 			c.gaugeFromNameAndValue(name, float64(metric.Value()))
 		case metrics.Histogram:
-			samples := metric.Snapshot().Sample().Values()
-			if len(samples) > 0 {
-				lastSample := samples[len(samples)-1]
-				c.gaugeFromNameAndValue(name, float64(lastSample))
-			}
+			snap := metric.Snapshot()
+			c.histogramFromName(name, snap)
 		case metrics.Meter:
 			lastSample := metric.Snapshot().Rate1()
 			c.gaugeFromNameAndValue(name, float64(lastSample))
