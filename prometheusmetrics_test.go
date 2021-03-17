64743030013d5618663a7fb4a0330812453d0a37
Nate Brown <nbrown.us@gmail.com>
2021-03-17 16:48:17 -0500

    Fix tests

--- prometheusmetrics_test.go
+++ prometheusmetrics_test.go
@@ -2,10 +2,12 @@ package prometheusmetrics
 
 import (
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/rcrowley/go-metrics"
 	"testing"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/rcrowley/go-metrics"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrometheusRegistration(t *testing.T) {
@@ -97,20 +99,63 @@ func TestPrometheusGaugeGetUpdated(t *testing.T) {
 func TestPrometheusMeterGetUpdated(t *testing.T) {
 	prometheusRegistry := prometheus.NewRegistry()
 	metricsRegistry := metrics.NewRegistry()
-	pClient := NewPrometheusProvider(metricsRegistry, "test", "subsys", prometheusRegistry, 1*time.Second)
+	pClient := NewPrometheusProvider(metricsRegistry, "test", "subsys", prometheusRegistry, time.Second)
 	gm := metrics.NewMeter()
 	metricsRegistry.Register("meter", gm)
 	gm.Mark(2)
 	go pClient.UpdatePrometheusMetrics()
-	gm.Mark(13)
-	time.Sleep(5 * time.Second)
+	gm.Mark(10)
+	time.Sleep(10 * time.Second)
+	gm.Mark(3)
 	metrics, _ := prometheusRegistry.Gather()
 	if len(metrics) == 0 {
 		t.Fatalf("prometheus was unable to register the metric")
 	}
-	serialized := fmt.Sprint(metrics[0])
-	expected := fmt.Sprintf("name:\"test_subsys_meter\" help:\"meter\" type:GAUGE metric:<gauge:<value:%.16f > > ", gm.Rate1())
-	if serialized != expected {
-		t.Fatalf("Go-metrics value and prometheus metrics value do not match")
+
+	assert.Equal(
+		t,
+		fmt.Sprintf("name:\"test_subsys_meter\" help:\"meter\" type:GAUGE metric:<label:<name:\"type\" value:\"count\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"rate1\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"rate15\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"rate5\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"rate_mean\" > gauge:<value:%v > > ", gm.Count(), gm.Rate1(), gm.Rate15(), gm.Rate5(), gm.RateMean()),
+		fmt.Sprint(metrics[0]),
+		"Go-metrics value and prometheus metrics value do not match",
+	)
+}
+
+func TestPrometheusHistogramGetUpdated(t *testing.T) {
+	prometheusRegistry := prometheus.NewRegistry()
+	metricsRegistry := metrics.NewRegistry()
+	pClient := NewPrometheusProvider(metricsRegistry, "test", "subsys", prometheusRegistry, 1*time.Second)
+	gm := metrics.NewHistogram(metrics.NewUniformSample(1028))
+	metricsRegistry.Register("metric", gm)
+
+	for ii := 0; ii < 94; ii++ {
+		gm.Update(1)
 	}
+	for ii := 0; ii < 5; ii++ {
+		gm.Update(5)
+	}
+	gm.Update(10)
+
+	go pClient.UpdatePrometheusMetrics()
+	time.Sleep(5 * time.Second)
+	metrics, _ := prometheusRegistry.Gather()
+
+	assert.Equal(
+		t,
+		fmt.Sprintf(
+			"name:\"test_subsys_metric\" help:\"metric\" type:GAUGE metric:<label:<name:\"type\" value:\"count\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"max\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"mean\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"min\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"perc75\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"perc95\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"perc99\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"perc999\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"stddev\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"sum\" > gauge:<value:%v > > metric:<label:<name:\"type\" value:\"variance\" > gauge:<value:%v > > ",
+			gm.Count(),
+			gm.Max(),
+			gm.Mean(),
+			gm.Min(),
+			gm.Percentile(75),
+			gm.Percentile(95),
+			gm.Percentile(99),
+			gm.Percentile(999),
+			gm.StdDev(),
+			gm.Sum(),
+			gm.Variance(),
+		),
+		fmt.Sprint(metrics[0]),
+		"Go-metrics value and prometheus metrics value do not match",
+	)
 }
